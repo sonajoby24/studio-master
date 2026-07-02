fbb8ace945eb9a07e8d9f5e71b9281baf8e603e7
Sona Joby <sonajoby24@gmail.com>
2026-07-02 11:23:31 +0530

    Fix Genkit build

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,12 +1,10 @@
-import { genkit } from 'genkit';
-import { openAI } from 'genkitx-openai';
+import { genkit } from "genkit";
+import { openAI } from "genkitx-openai";
 
 export const ai = genkit({
   plugins: [
     openAI({
-      apiKey: process.env.OPENAI_API_KEY,
+      apiKey: process.env.OPENAI_API_KEY!,
     }),
   ],
-
-  model: openAI.model('gpt-4o-mini'),
 });
